5df48095ec95965b9ae2c782307f928116427efb
Reuben <grerlich@gmail.com>
2024-10-03 17:51:31 -0400

    problem 3

--- server.js
+++ server.js
@@ -16,6 +16,23 @@ app.get('/roll/:number', (req, res) => {
     }
     });
     
+    const collectibles = [
+        { name: 'shiny ball', price: 5.95 },
+        { name: 'autographed picture of a dog', price: 10 },
+        { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+    ]
+ app.get('/collectibles/:index', (req,res) => {
+    const index = parseInt(req.params.index);
+    if (isNaN(index) || index < 0 ) {
+     res.send("This is not a valid entry, please try again.");
+    }
+    if (index >= collectibles.length) {
+     res.send("This item is not in stock at the moment, please check back soon!");
+    } else {
+        let item = collectibles[index];
+        res.send(`So, you want the ${item.name} ? For ${item.price} dollars it can be yours`);
+    }
+ });
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
