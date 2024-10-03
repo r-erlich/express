a5e4419d28692ab5b4d29b682b9240fb449bbdba
Reuben <grerlich@gmail.com>
2024-10-03 17:40:11 -0400

    second question

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ app.get('/greetings/:name', (req, res) => {
     res.send(`Greetings ${req.params.name} I hope you evening is good`)
 });
 
+app.get('/roll/:number', (req, res) => {
+    const number = parseInt(req.params.number);
+    if (isNaN(number)) {
+        res.send('Please specify a number');
+    } else {
+        const rolledNumber = Math.floor(Math.random() * (number + 1));
+        res.send(`You rolled a ${rolledNumber}.`);
+    }
+    });
+    
+
 app.listen(3000, () => {
     console.log('Listening on port 3000')
 });
